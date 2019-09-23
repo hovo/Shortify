89d77e332fd8958e4b839f13a57852ff044f4d90
Hovhannes Khachikyan <hovo.khc@gmail.com>
2019-09-23 16:07:24 -0400

    Update base62_test.go

--- base62/base62_test.go
+++ base62/base62_test.go
@@ -23,3 +23,25 @@ func TestEncode(t *testing.T) {
 		}
 	}
 }
+
+func TestDecode(t *testing.T) {
+	testData := []struct {
+		key      string
+		expected uint64
+	}{
+		{"0", 0},
+		{"a", 10},
+		{"aa", 630},
+		{"abc123EFG", 2222821365901088},
+		{"hjNv8tS3K", 3781504209452600},
+	}
+	for _, testCase := range testData {
+		n, err := Decode(testCase.key)
+		if n != testCase.expected {
+			t.Fatalf("expected %v, but got %v", testCase.expected, n)
+		}
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	}
+}
